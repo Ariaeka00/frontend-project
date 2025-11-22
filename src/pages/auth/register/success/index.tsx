233a260e32528090e9c5ac1f0c7d53824dc3158d
Ariaeka00 <ariaeka089@gmail.com>
2025-11-23 00:13:49 +0700

    feat: create success page

--- src/pages/auth/register/success/index.tsx
+++ src/pages/auth/register/success/index.tsx
@@ -1,10 +1,10 @@
 import AuthLayout from "@/components/layouts/AuthLayout";
-import Register from "@/components/views/Register";
+import RegisterSuccess from "@/components/views/RegisterSuccess/RegisterSuccess";
 
 const RegisterSuccessPage = () => {
     return(
         <AuthLayout title="Master Pack Indonesia | Register Success">
-            <h1>Register Success</h1>
+            <RegisterSuccess />
         </AuthLayout>
     )
 }
